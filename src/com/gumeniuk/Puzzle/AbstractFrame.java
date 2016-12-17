95cfe9f87203879220c739d310d65db0f7aecdaf
Stan Gumeniuk <i@vigo.su>
2016-12-17 11:07:09 +0300

    [add]

--- src/com/gumeniuk/Puzzle/AbstractFrame.java
+++ src/com/gumeniuk/Puzzle/AbstractFrame.java
@@ -2,16 +2,63 @@ package com.gumeniuk.Puzzle;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
+import java.awt.event.*;
 
 public abstract class AbstractFrame extends JFrame {
-    private class WindowEventListener implements WindowAdapter {
+    public AbstractFrame() throws HeadlessException {
+        actionListenerImpl = new ActionListenerImpl();
 
+        WindowEventListener windowEventListener = new WindowEventListener();
+        addWindowListener(windowEventListener);
+        addWindowStateListener(windowEventListener);
+        addWindowFocusListener(windowEventListener);
+    }
+
+    private class WindowEventListener extends WindowAdapter {
+
+        @Override
+        public void windowOpened(WindowEvent e) {
+            onInit();
+        }
+
+        @Override
+        public void windowClosed(WindowEvent e) {
+            onClose();
+        }
 
     }
-    public AbstractFrame() throws HeadlessException {
-        addWindowListener();
+
+    private class ActionListenerImpl implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            Object source = e.getSource();
+
+            if (source instanceof JButton) {
+                onButtonClick((JButton) source);
+            } else {
+                onAction(source, e.getActionCommand());
+            }
+        }
+
+    }
+
+    private final ActionListenerImpl actionListenerImpl;
+
+    public ActionListenerImpl getActionListenerImpl() {
+        return actionListenerImpl;
+    }
+
+    protected void onInit() {
+    }
+
+    protected void onClose() {
+    }
+
+    protected void onAction(Object src, String cmd) {
+
+    }
+
+    protected void onButtonClick(JButton btn) {
+
     }
 }
